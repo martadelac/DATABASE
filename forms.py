04d6821c5d0592d74d03a343b96cfb4a33894718
martadelac <62816624+martadelac@users.noreply.github.com>
2022-01-07 06:26:51 -0600

    Add files via upload
    FILES for DATABASE DJ

---
+++ forms.py
@@ -0,0 +1,38 @@
+"""Forms for playlist app."""
+
+from wtforms import SelectField
+from flask_wtf import FlaskForm
+from wtforms.fields.core import StringField, validators
+from wtforms.fields.simple import TextAreaField
+from wtforms.widgets.core import TextArea
+
+
+class PlaylistForm(FlaskForm):
+    """Form for adding playlists."""
+
+    # Add the necessary code to use this form
+
+    name = StringField(
+        "Name", validators=[validators.DataRequired()]), 
+
+    description = TextAreaField(
+        "Description")
+
+
+class SongForm(FlaskForm):
+    """Form for adding songs."""
+
+    title = StringField(
+        "Title", validators=[validators.DataRequired()]) 
+
+    artist = StringField(
+        "Artist", validators=[validators.DataRequired()])
+
+    # Add the necessary code to use this form
+
+
+# DO NOT MODIFY THIS FORM - EVERYTHING YOU NEED IS HERE
+class NewSongForPlaylistForm(FlaskForm):
+    """Form for adding a song to playlist."""
+
+    song = SelectField('Song To Add', coerce=int)
---
+++ models.py
@@ -0,0 +1,49 @@
+"""Models for Playlist app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Playlist(db.Model):
+    """Playlist."""
+        # ADD THE NECESSARY CODE HERE
+
+
+    __tablename__="playlists"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+
+
+class Song(db.Model):
+    """Song."""
+
+    # ADD THE NECESSARY CODE HERE
+
+    __tablename__="songs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    artist = db.Column(db.Text, nullable=False)
+
+
+class PlaylistSong(db.Model):
+    """Mapping of a playlist to a song."""
+
+    # ADD THE NECESSARY CODE HERE
+    __tablename__="playlists_songs"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.Text, nullable=False)
+    artist = db.Column(db.Text, nullable=False)
+
+
+# DO NOT MODIFY THIS FUNCTION
+def connect_db(app):
+    """Connect to database."""
+
+    db.app = app
+    db.init_app(app)
